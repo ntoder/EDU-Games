58ec3edad705ac3ddd99dce6ccceb2ef620ce8a5
Noam Toder Yoga <noam@rbscode.com>
2026-04-06 11:47:34 -0700

    modified:   Math Balloon Pop/script.js

--- Math Balloon Pop/script.js
+++ Math Balloon Pop/script.js
@@ -2,13 +2,19 @@ let balloonEl=document.createElement("div")
 balloonEl.className="balloon"
 balloonEl.innerText ="7"
 document.body.appendChild(balloonEl)
-function createBalloon(number, colour, speed, position){
+let position={left:100, top:window.innerHeight}
+function createBalloon(number, colour, speed){
     let balloonEl=document.createElement("div")
     balloonEl.className="balloon"
     balloonEl.innerText =number    
+    balloonEl.style.color=colour
+    position.top-=speed
+    balloonEl.style.top=position.top
+    balloonEl.style.left=position.left
+
     document.body.appendChild(balloonEl)
 }
 setInterval(moveBalloon, 20)
 function moveBalloon(){
-    createBalloon
+    createBalloon("8", "red", 2 )
 }
