ae5465d53586a5c757a15b64ac0c7f828bb3c0d5
Noam Toder Yoga <noam@rbscode.com>
2026-04-06 11:32:08 -0700

    new file:   Math Balloon Pop/SuperStarfish.ttf 	modified:   Math Balloon Pop/index.html 	modified:   Math Balloon Pop/script.js 	modified:   Math Balloon Pop/style.css

--- Math Balloon Pop/script.js
+++ Math Balloon Pop/script.js
@@ -5,6 +5,6 @@ document.body.appendChild(balloonEl)
 function createBalloon(number, colour, speed, position){
     let balloonEl=document.createElement("div")
     balloonEl.className="balloon"
-    balloonEl.innerText ="7"
+    balloonEl.innerText =number    
     document.body.appendChild(balloonEl)
 }
